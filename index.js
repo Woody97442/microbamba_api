18ed15d11389c05f8336a9cdb9841e46f86be9cb
brianosipro <woody97442@hotmail.fr>
2024-05-15 12:45:07 +0400

    up

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const app = express();
 // app.use(cors());
 // app.use("/image", express.static("public/image"));
 
-// // Routes
-// const productRoutes = require("./routes/product.routes");
-// app.use("/", productRoutes);
+// Routes
+const productRoutes = require("./routes/product.routes");
+app.use("/", productRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
