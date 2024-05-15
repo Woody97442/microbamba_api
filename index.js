7720d69396cac17d7f7bb388574cab7710666022
brianosipro <woody97442@hotmail.fr>
2024-05-15 12:21:34 +0400

    update vercel

--- index.js
+++ index.js
@@ -16,8 +16,3 @@ app.use("/image", express.static("public/image"));
 // Routes
 const productRoutes = require("./routes/product.routes");
 app.use("/", productRoutes);
-
-// Server
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
