3530f5b0b3d39b5052378808eb89b51a8d9eecfe
brianosipro <woody97442@hotmail.fr>
2024-05-15 12:43:12 +0400

    relaod

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-// Express js
+// // Express js
 const express = require("express");
 const app = express();
-const cors = require("cors");
+// const cors = require("cors");
 
-// dotenv
-require("dotenv").config({ path: "./.env.local" });
+// // dotenv
+// require("dotenv").config({ path: "./.env.local" });
 
-// db connect
-require("./config/db");
+// // db connect
+// require("./config/db");
 
-// Middlewares
-app.use(cors());
-app.use("/image", express.static("public/image"));
+// // Middlewares
+// app.use(cors());
+// app.use("/image", express.static("public/image"));
 
-// Routes
-const productRoutes = require("./routes/product.routes");
-app.use("/", productRoutes);
+// // Routes
+// const productRoutes = require("./routes/product.routes");
+// app.use("/", productRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 module.exports = app;
